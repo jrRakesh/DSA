43d8b8b1f23648b79da1ec0f727020c66098522e
jrRakesh <reetsnp@gmail.com>
2026-07-03 01:43:26 +0545

    sorting algorithms in python

--- Sorting/sorting.py
+++ Sorting/sorting.py
@@ -1,5 +1,6 @@
 import random
 import time
+import matplotlib.pyplot as plt
 
 def swap(a, b):
     temp = a
@@ -13,21 +14,25 @@ def display(arr):
 
 def selection_sort(arr):
     n = len(arr)
-    for i in range(n - 1):
-        least = arr[i]
+    for i in range(n-1):
+        min = arr[i]
         pos = i
-        for j in range(i + 1, n):
-            if arr[j] < least:
-                least = arr[j]
+
+        for j in range(i+1, n):
+            if arr[j] < min:
+                min = arr[j]
                 pos = j
-        if i != pos:
-            swap(arr[i], arr[pos])
+        if( i != pos):
+            arr[i], arr[pos] = arr[pos], arr[i]
+
+    
 
 
 n = int(input("Enter n: "))
 A = [random.randint(0, 99999) for _ in range(n)]
 
 # display before sorting
+display(A)
 start_time = time.time()
 
 selection_sort(A)
@@ -36,3 +41,10 @@ end_time = time.time()
 display(A)  # display after sorting
 
 print(f"Time taken is {end_time - start_time} seconds")
+
+time_taken = end_time - start_time
+timeTaken = []
+for i in range():
+    timeTaken = i
+plt.plot(timeTaken, A)
+plt.show()
