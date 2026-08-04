e3fb3363de6329b9dc7d5e21c9307ae992050870
Rakesh Mehta <reetsnp@gmail.com>
2026-08-04 19:55:17 +0545

    structuring folder

---
+++ Warshall.py
@@ -0,0 +1,19 @@
+import copy
+
+G = [
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+    [0, 0, 0, 0],
+]
+def warshall(G):
+    n = len(G)
+    W = copy.deepcopy(G)
+    for k in range(0, n):
+        for i in range(0, n):
+            for j in range(0, n):
+                W[i][j] = W[i][j] or (W[i][k] and W[k][j])
+    return W
+answer = warshall(G)
+for row in answer:
+    print(row)
